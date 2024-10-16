98c90fd9e9802543efd77634b216ecf1f6c52e02
Arthur Wallendorff <howdy@wallenart.dev>
2024-10-16 23:49:22 +0200

    fix: select proper prettier parser + remove non-existing filetypes

--- lua/configs/lsp/format-buffer.lua
+++ lua/configs/lsp/format-buffer.lua
@@ -3,150 +3,153 @@ local ux = require("utils.ux")
 local formatters = require("utils.formatters")
 
 local clang_types = {
-	filetypes.c,
-	filetypes.cpp,
-	filetypes.arduino,
+    filetypes.c,
+    filetypes.cpp,
+    filetypes.arduino,
 }
 
 local prettier_types = {
-	filetypes.javascript,
-	filetypes.javascriptreact,
-	filetypes.typescript,
-	filetypes.typescriptreact,
-	filetypes.jsx,
-	filetypes.tsx,
-	filetypes.json,
-	filetypes.css,
+    filetypes.javascript,
+    filetypes.typescript,
+    filetypes.jsx,
+    filetypes.tsx,
+    filetypes.json,
+    filetypes.css,
 }
 
 --- Format current buffer based on filetype. Fallbacks to nvim-lsp formatter.
 return function()
-	local filetype = vim.bo.filetype
+    local filetype = vim.bo.filetype
 
-	-- [[ C/C++/Arduino ]]
-	if vim.tbl_contains(clang_types, filetype) then
-		if vim.fn.executable("clang-format") ~= 1 then
-			vim.notify("clang-format is not installed, using LSP formatter", vim.log.levels.ERROR)
-			goto FALLBACK
-		end
+    -- [[ C/C++/Arduino ]]
+    if vim.tbl_contains(clang_types, filetype) then
+        if vim.fn.executable("clang-format") ~= 1 then
+            vim.notify("clang-format is not installed, using LSP formatter", vim.log.levels.ERROR)
+            goto FALLBACK
+        end
 
-		local current_file_name = vim.fn.expand("%")
-		local cmd = { "clang-format", current_file_name }
-		_ = formatters.run_command_on_buffer(cmd)
+        local current_file_name = vim.fn.expand("%")
+        local cmd = { "clang-format", current_file_name }
+        _ = formatters.run_command_on_buffer(cmd)
 
-		return
-	end
+        return
+    end
 
-	-- [[ Go ]]
-	if filetype == filetypes.go then
-		if vim.fn.executable("gofumpt") ~= 1 then
-			vim.notify("gofumpt is not installed, using LSP formatter", vim.log.levels.ERROR)
-			goto FALLBACK
-		end
+    -- [[ Go ]]
+    if filetype == filetypes.go then
+        if vim.fn.executable("gofumpt") ~= 1 then
+            vim.notify("gofumpt is not installed, using LSP formatter", vim.log.levels.ERROR)
+            goto FALLBACK
+        end
 
-		local current_file_name = vim.fn.expand("%")
-		local cmd = { "gofumpt", current_file_name }
+        local current_file_name = vim.fn.expand("%")
+        local cmd = { "gofumpt", current_file_name }
 
-		local ok = formatters.run_command_on_buffer(cmd)
+        local ok = formatters.run_command_on_buffer(cmd)
 
-		if not ok then
-			return
-		end
+        if not ok then
+            return
+        end
 
-		cmd = {
-			"golines",
-			current_file_name,
-			"--max-len=80",
-			"--ignore-generated",
-			"--ignored-dirs=vendor,node_modules",
-		}
+        cmd = {
+            "golines",
+            current_file_name,
+            "--max-len=80",
+            "--ignore-generated",
+            "--ignored-dirs=vendor,node_modules",
+        }
 
-		_ = formatters.run_command_on_buffer(cmd)
+        _ = formatters.run_command_on_buffer(cmd)
 
-		return
-	end
+        return
+    end
 
-	-- [[ Lua ]]
-	if filetype == filetypes.lua then
-		local ok, stylua = pcall(require, "stylua-nvim")
+    -- [[ Lua ]]
+    if filetype == filetypes.lua then
+        local ok, stylua = pcall(require, "stylua-nvim")
 
-		if not ok then
-			vim.notify("Stylua is not installed, using LSP formatter", vim.log.levels.ERROR)
-			goto FALLBACK
-		end
+        if not ok then
+            vim.notify("Stylua is not installed, using LSP formatter", vim.log.levels.ERROR)
+            goto FALLBACK
+        end
 
-		stylua.format_file()
+        stylua.format_file()
 
-		return
-	end
+        return
+    end
 
-	-- [[ JS/TS/Json/CSS ]]
-	if vim.tbl_contains(prettier_types, filetype) then
-		if vim.fn.executable("prettierd") ~= 1 then
-			vim.notify("Prettierd is not installed, using LSP formatter", vim.log.levels.ERROR)
-			goto FALLBACK
-		end
+    -- [[ JS/TS/Json/CSS ]]
+    if vim.tbl_contains(prettier_types, filetype) then
+        if vim.fn.executable("prettierd") ~= 1 then
+            vim.notify("Prettierd is not installed, using LSP formatter", vim.log.levels.ERROR)
+            goto FALLBACK
+        end
 
-		local current_file_name = vim.fn.expand("%:t")
-		local cmd = { "prettierd", "--no-color", current_file_name }
-		local ok = formatters.run_command_on_buffer(cmd)
+        local parser = "babel"
+        if string.find(filetype, "typescript") then
+            parser = parser .. "-ts"
+        end
 
-		if not ok then
-			goto FALLBACK
-		end
+        local current_file_name = vim.fn.expand("%")
+        local cmd = { "prettierd", "--no-color", "--parser=" .. parser, current_file_name }
+        local ok = formatters.run_command_on_buffer(cmd)
 
-		return
-	end
+        if not ok then
+            goto FALLBACK
+        end
 
-	-- [[ WebC/Markdown ]]
-	if filetype == filetypes.webc or filetype == filetypes.markdown then
-		formatters.lsp_format_skip_frontmatter()
+        return
+    end
 
-		return
-	end
+    -- [[ WebC/Markdown ]]
+    if filetype == filetypes.webc or filetype == filetypes.markdown then
+        formatters.lsp_format_skip_frontmatter()
 
-	-- [[ templ ]]
-	if filetype == filetypes.templ then
-		if vim.fn.executable("templ") ~= 1 then
-			vim.notify("templ is not installed, using LSP formatter", vim.log.levels.ERROR)
-			goto FALLBACK
-		end
+        return
+    end
 
-		local current_file_name = vim.fn.expand("%")
-		local cmd = {
-			"templ",
-			"fmt",
-			"-stdout",
-			"-log-level",
-			"error",
-			current_file_name,
-		}
-		_ = formatters.run_command_on_buffer(cmd)
+    -- [[ templ ]]
+    if filetype == filetypes.templ then
+        if vim.fn.executable("templ") ~= 1 then
+            vim.notify("templ is not installed, using LSP formatter", vim.log.levels.ERROR)
+            goto FALLBACK
+        end
 
-		return
-	end
+        local current_file_name = vim.fn.expand("%")
+        local cmd = {
+            "templ",
+            "fmt",
+            "-stdout",
+            "-log-level",
+            "error",
+            current_file_name,
+        }
+        _ = formatters.run_command_on_buffer(cmd)
 
-	-- [[ Nix ]]
-	if filetype == filetypes.nix then
-		if vim.fn.executable("nixfmt") ~= 1 then
-			vim.notify("nixfmt is not installed, skipping formatting", vim.log.levels.ERROR)
-			return
-		end
+        return
+    end
 
-		-- FIXME: outputs an error about a non existing file (but it still works?)
-		-- it seems the (temp?) file does not exist the first time we format
-		-- then it's fine for all files
-		local current_file_name = vim.fn.expand("%")
-		local cmd = {
-			"nixfmt <",
-			"-w 80",
-			current_file_name,
-		}
+    -- [[ Nix ]]
+    if filetype == filetypes.nix then
+        if vim.fn.executable("nixfmt") ~= 1 then
+            vim.notify("nixfmt is not installed, skipping formatting", vim.log.levels.ERROR)
+            return
+        end
 
-		_ = formatters.run_command_on_buffer(cmd)
-	end
+        -- FIXME: outputs an error about a non existing file (but it still works?)
+        -- it seems the (temp?) file does not exist the first time we format
+        -- then it's fine for all files
+        local current_file_name = vim.fn.expand("%")
+        local cmd = {
+            "nixfmt <",
+            "-w 80",
+            current_file_name,
+        }
 
-	-- [[ Fallback ]]
-	::FALLBACK::
-	vim.lsp.buf.format()
+        _ = formatters.run_command_on_buffer(cmd)
+    end
+
+    -- [[ Fallback ]]
+    ::FALLBACK::
+    vim.lsp.buf.format()
 end
