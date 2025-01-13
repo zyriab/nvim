5490abe9d348e29a0554e23313d433ab17bcc778
Arthur Wallendorff <howdy@wallenart.dev>
2025-01-13 21:02:27 +0100

    chore: use lsp-notify repo

--- lua/plugins/ui/nvim-lsp-notify.lua
+++ lua/plugins/ui/nvim-lsp-notify.lua
@@ -1,7 +1,7 @@
 -- nicer lsp notifications using nvim-notify
 return {
-    -- "mrded/nvim-lsp-notify",
-    dir = "$HOME/Developer/nvim-plugins/nvim-lsp-notify",
+    "mrded/nvim-lsp-notify",
+    -- dir = "$HOME/Developer/nvim-plugins/nvim-lsp-notify",
     dependencies = { "rcarriga/nvim-notify" },
     config = function() end,
     opts = {
