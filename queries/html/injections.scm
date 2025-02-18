0fb1050b3176866a163b442437c181f8507bfabd
Arthur Wallendorff <howdy@wallenart.dev>
2025-02-18 18:39:19 +0100

    feat: match alpinejs attributes on @ too

--- queries/html/injections.scm
+++ queries/html/injections.scm
@@ -2,8 +2,8 @@
 
 ; AlpineJS attributes
 (attribute
-  (attribute_name) @_attr
-    (#lua-match? @_attr "^x%-%l")
-  (quoted_attribute_value
-    (attribute_value) @injection.content)
-  (#set! injection.language "javascript"))
+    (attribute_name) @_attr
+        (#lua-match? @_attr "^[@x][%-:]?%l")
+    (quoted_attribute_value
+        (attribute_value) @injection.content)
+    (#set! injection.language "javascript"))
