db9d67939c56bdcc8120cb4f2e429c74378672ca
Arthur Wallendorff <howdy@wallenart.dev>
2025-03-08 20:50:38 +0100

    feat: use better sql formatter

--- lua/configs/lsp/format-buffer.lua
+++ lua/configs/lsp/format-buffer.lua
@@ -162,37 +162,26 @@ return function()
 
     -- [[ SQL ]]
     if filetype == filetypes.sql then
-        if vim.fn.executable("sleek") ~= 1 then
-            vim.notify("sleek is not installed, skipping formatting", vim.log.levels.ERROR)
+        if vim.fn.executable("pg_format") ~= 1 then
+            vim.notify("pg_format is not installed, skipping formatting", vim.log.levels.ERROR)
             return
         end
 
-        local bufnr = vim.api.nvim_get_current_buf()
-        local temp_file_name = vim.fn.tempname()
+        local current_file_name = vim.fn.expand("%")
+        local cmd = {
+            "pg_format",
+            "-w",
+            "80",
+            "--function-case",
+            "1",
+            "--type-case",
+            "2",
+            "--no-space-function",
+            "--no-rcfile",
+            current_file_name,
+        }
 
-        vim.api.nvim_buf_call(bufnr, function()
-            vim.cmd("silent noa write " .. vim.fn.fnameescape(temp_file_name))
-        end)
-
-        vim.cmd("silent !sleek " .. vim.fn.fnameescape(temp_file_name))
-
-        local temp_file = io.open(temp_file_name, "r")
-
-        if temp_file == nil then
-            vim.notify_once("Failed to read formatted file", vim.log.levels.ERROR)
-
-            return
-        end
-
-        local temp_content = temp_file:read("*all")
-        local formatted = vim.split(temp_content, " ")
-        formatted = vim.split(temp_content, "\n")
-
-        temp_file:close()
-
-        os.remove(temp_file_name)
-
-        vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, formatted)
+        formatters.run_command_on_buffer(cmd)
 
         return
     end
