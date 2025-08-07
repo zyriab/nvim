a5e5deb19bca5e577599f96ca0e7ee95f29cdd68
Arthur Wallendorff <howdy@wallenart.dev>
2025-08-07 18:42:33 +0200

    feat: add more hyperscript support

--- queries/html/injections.scm
+++ queries/html/injections.scm
@@ -8,6 +8,14 @@
         (attribute_value) @injection.content)
     (#set! injection.language "javascript"))
 
+; hx-vals attribute (using Javascript)
+(attribute
+    (attribute_name) @_attr
+        (#eq? @_attr "hx-vals")
+    (quoted_attribute_value
+        (attribute_value) @injection.content)
+    (#set! injection.language "javascript"))
+
 ; _hyperscript attribute
 (attribute
     (attribute_name) @_attr
@@ -16,7 +24,7 @@
         (attribute_value) @injection.content)
     (#set! injection.language "hyperscript"))
 
-; _hyperscript attribute in Go's `fmt.Sprintf`
+; _hyperscript attribute in Go's `fmt.Sprintf` (quotes)
 (attribute
     (attribute_name) @_attr
         (#eq? @_attr "_")
@@ -32,7 +40,7 @@
     (#eq? @_sprintf "Sprintf")
     (#set! injection.language "hyperscript"))
 
-; _hyperscript attribute in Go's `fmt.Sprintf` (raw string literals - backticks)
+; _hyperscript attribute in Go's `fmt.Sprintf` (backticks)
 (attribute
     (attribute_name) @_attr
         (#eq? @_attr "_")
@@ -48,10 +56,57 @@
     (#eq? @_sprintf "Sprintf")
     (#set! injection.language "hyperscript"))
 
-; hx-vals attribute (using Javascript)
-(attribute
-    (attribute_name) @_attr
-        (#eq? @_attr "hx-vals")
-    (quoted_attribute_value
-        (attribute_value) @injection.content)
-    (#set! injection.language "javascript"))
+; HS field with fmt.Sprintf (quotes)
+(keyed_element
+    key: (literal_element
+        (identifier) @_key)
+    value: (literal_element
+        (call_expression
+            function: (selector_expression
+                operand: (identifier) @_fmt
+                field: (field_identifier) @_sprintf)
+            arguments: (argument_list
+                (interpreted_string_literal
+                    (interpreted_string_literal_content) @injection.content))))
+    (#eq? @_key "HS")
+    (#eq? @_fmt "fmt")
+    (#eq? @_sprintf "Sprintf")
+    (#set! injection.language "hyperscript"))
+
+; HS field with fmt.Sprintf (backticks)
+(keyed_element
+    key: (literal_element
+        (identifier) @_key)
+    value: (literal_element
+        (call_expression
+            function: (selector_expression
+                operand: (identifier) @_fmt
+                field: (field_identifier) @_sprintf)
+            arguments: (argument_list
+                (raw_string_literal
+                    (raw_string_literal_content) @injection.content))))
+    (#eq? @_key "HS")
+    (#eq? @_fmt "fmt")
+    (#eq? @_sprintf "Sprintf")
+    (#set! injection.language "hyperscript"))
+
+; HS field with interpreted string literal (quotes)
+(keyed_element
+    key: (literal_element
+        (identifier) @_key)
+    value: (literal_element
+        (interpreted_string_literal
+            (interpreted_string_literal_content) @injection.content))
+    (#eq? @_key "HS")
+    (#set! injection.language "hyperscript"))
+
+; HS field with raw string literal (backticks)
+(keyed_element
+    key: (literal_element
+        (identifier) @_key)
+    value: (literal_element
+        (raw_string_literal
+            (raw_string_literal_content) @injection.content))
+    (#eq? @_key "HS")
+    (#set! injection.language "hyperscript"))
+
